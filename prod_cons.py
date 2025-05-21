2c91ccd807b7f77578720a8d8b89a0a662e9f904
MauricioCa07 <carrillomauricio007@gmail.com>
2025-05-21 16:17:03 -0500

    comments to explain what im doing

--- prod_cons.py
+++ prod_cons.py
@@ -1,4 +1,6 @@
+#using this beacuse the teacher used this and it manages the order
 from queue import Queue
+
 
 class GenProdCons:
     def __init__(self, size=10):
@@ -12,9 +14,8 @@ class GenProdCons:
         return self.q.get() 
 
 
-
     def is_full(self):
-        return self.q.qsize() == self.size
+        return self.q.full()
     
     def is_empty(self):
         return self.q.empty()
@@ -22,6 +23,7 @@ class GenProdCons:
     def size(self):
         return self.size
     
-
+    #We override the len() method bc the teacher 
+    #was using it. I dont want to change the tests
     def __len__(self):
         return self.q.qsize()
